109d400e36b8a45494cea422822c6e4b04aac288
EC2 Default User <ec2-user@ip-172-31-40-168.us-east-2.compute.internal>
2021-02-17 03:31:56 +0000

    part 2 complete!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   # end
   
   def index
-    @all_ratings = Movie.all_ratings
+    # @all_ratings = Movie.all_ratings
     
     # # current setting from params or session
     # sort = params[:sort] || session[:sort]
