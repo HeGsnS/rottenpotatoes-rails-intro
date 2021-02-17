5a7888173496422969e283d5d90eaf79c59e3590
EC2 Default User <ec2-user@ip-172-31-40-168.us-east-2.compute.internal>
2021-02-17 12:27:07 +0000

    complete part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,36 +11,36 @@ class MoviesController < ApplicationController
   # end
   
   def index
-    # @all_ratings = Movie.all_ratings
+    @all_ratings = Movie.all_ratings
     
-    # # current setting from params or session
-    # sort = params[:sort] || session[:sort]
-    # @checked_ratings = params[:ratings] || session[:ratings] \
-    #   || Hash[@all_ratings.map { |r| [r, 1] }]
+    # current setting from params or session
+    sort = params[:sort] || session[:sort]
+    @ratings_to_show = params[:ratings] || session[:ratings] \
+      || Hash[@all_ratings.map { |r| [r, 1] }]
     
-    # # redirect_to
-    # if !params[:commit].nil? or params[:ratings].nil? or \
-    #   (params[:sort].nil? && !session[:sort].nil?)
-    #   flash.keep
-    #   redirect_to movies_path :sort => sort, :ratings => @checked_ratings
-    # end
+    # redirect_to
+    if !params[:commit].nil? or params[:ratings].nil? or \
+      (params[:sort].nil? && !session[:sort].nil?)
+      flash.keep
+      redirect_to movies_path :sort => sort, :ratings => @ratings_to_show
+    end
     
-    # # the toggled column
-    # case sort
-    # when 'title'
-    #   ordering, @title_cls = {:title => :asc}, 'hilite'
-    # when 'release_date'
-    #   ordering, @release_cls = {:release_date => :asc}, 'hilite'
-    # end
+    # the toggled column
+    case sort
+    when 'title'
+      ordering, @title_cls = {:title => :asc}, 'hilite'
+    when 'release_date'
+      ordering, @release_cls = {:release_date => :asc}, 'hilite'
+    end
     
-    # # movies from Movie
-    # @movies = Movie.with_ratings(@checked_ratings.keys).order(ordering)
+    # movies from Movie
+    @movies = Movie.with_ratings(@ratings_to_show.keys).order(ordering)
 
-    # # current setting to session
-    # session[:sort] = sort
-    # session[:ratings] = @checked_ratings
+    # current setting to session
+    session[:sort] = sort
+    session[:ratings] = @ratings_to_show
     
-    @movies = Movie.all
+    # @movies = Movie.all
   end
 
   def new
