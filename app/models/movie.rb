5194c504055d3278d8f92b074b38a7f426693bcf
EC2 Default User <ec2-user@ip-172-31-40-168.us-east-2.compute.internal>
2021-02-17 13:21:53 +0000

    complete part 3.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,9 @@
 class Movie < ActiveRecord::Base
+    # all_ratings method
     def self.all_ratings
         pluck(:rating).uniq
     end
+    # with_ratings method
     def self.with_ratings(ratings)
         where("LOWER(rating) IN (?)", ratings.map(&:downcase))
     end
